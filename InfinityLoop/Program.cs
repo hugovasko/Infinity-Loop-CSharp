5ac76d1824180d5e9602befff0b3964db7c7cc4d
hugovasko <hugovasko1@gmail.com>
2022-10-16 14:08:46 +0300

    Project init

---
+++ InfinityLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InfinityLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i >= 1 && i<= 23666; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
